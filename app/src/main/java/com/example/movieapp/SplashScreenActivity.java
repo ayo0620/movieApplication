e3adba979cbeaff06876c1e6e42acd3d16820fda
ayo0620 <aayomide491@gmail.com>
2022-09-06 14:33:28 -0500

    base code

---
+++ app/src/main/java/com/example/movieapp/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation zoom;
+    ImageView imageAnim;
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        imageAnim = findViewById(R.id.imageAnim);
+        imageAnim.setAnimation(zoom);
+        getSupportActionBar().hide();
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
